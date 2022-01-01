597fa81ae3f8592bad22f8a08650d26e9ec40cc3
IlyasBouziane <ilyas.bouziane@um5s.net.ma>
2022-01-01 17:43:28 +0100

    init project

---
+++ src/SudokuSolver.java
@@ -0,0 +1,9 @@
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ISolver;
+
+public class SudokuSolver {
+    public static ISolver solver;
+    public static void main(String[] args) {
+
+    }
+}
